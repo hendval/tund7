728834a843a5df6887e9c6fb54dc7320c5c0335e
Hendrik Vallimägi <hendval@greeny.cs.tlu.ee>
2015-10-14 10:20:52 +0300

    lisasin failid

---
+++ functions.php
@@ -0,0 +1,75 @@
+<?php
+	require_once("../tund5/configGlobal.php");
+	$database = "if15_hendval";
+	
+	
+	function getCarData(){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+	
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color from car_plates WHERE deleted IS NULL");
+		$stmt->bind_result($id, $user_id_from_database, $number_plate, $color);
+		$stmt->execute();
+		
+		// tekitan tühja massiivi ,kus edaspidi hoian objekte
+		$array = array();
+		
+		//tee midagi seni kuni saame ab'st ühe rea andmeid
+		while($stmt->fetch()){
+			//while statement yo
+			// tekitan objekti kus hakkan hoidma väärtusi
+			$car = new StdClass();
+			$car->id = $id;
+			$car->plate = $number_plate;
+			$car->color = $color;
+			$car->userid = $user_id_from_database;
+			
+			//lisan massiivi
+			array_push($array, $car);
+			//var dump ütleb muutuja tüübi ja sisu
+			//echo "<pre>";
+			//var_dump($array);
+			//echo "</pre><br>";
+			
+		}
+		
+		//tagastn massiivi
+		return $array;
+		
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	function deleteCar($id){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id);
+		if($stmt->execute()){
+			//sai kustutatud
+			//kustutame aadressirea tühjaks
+			header("Location: table.php");
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	
+	function updateCar($id, $number_plate, $color){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET number_plate=?, color=? WHERE id=?");
+		$stmt->bind_param("ssi", $number_plate, $color, $id);
+		if($stmt->execute()){
+			//sai kustutatud
+			//kustutame aadressirea tühjaks
+			//header("Location: table.php");
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	//käivitan funktsiooni
+	getCarData();
+?>
---
+++ table.php
@@ -0,0 +1,68 @@
+<?php
+	require_once("functions.php");
+	
+	//kas kustutame
+	if(isset($_GET["delete"])){
+		
+		echo "Kustutame id"." ".$_GET["delete"];
+		// käivitan funktsiooni, saadan kaasa id
+		deleteCar($_GET["delete"]);
+		
+	}
+	
+	
+	//salvestan ab'i
+	if(isset($_POST["Save"])){
+	
+		updateCar($_POST["id"], $_POST["number_plate"], $_POST["color"]);
+	
+	}
+	
+	$array = getCarData();
+	
+?>
+<h2>Tabel</h2>
+<table border=1>
+	<tr>
+		<th>ID</th>
+		<th>User ID</th>
+		<th>Numbrimärk</th>
+		<th>Värv</th>
+		<th>Kustuta</th>
+		<th>Muuda</th>
+	</tr>
+	
+	<?php 
+	for($i = 0; $i < count($array); $i++){
+		
+		//kasutaja tahab muuta
+		if(isset($_GET["edit"]) && $array[$i]->id == $_GET["edit"]){
+			
+			echo "<tr>";
+			echo "<form action='table.php' method='post'>";
+			echo "<input type='hidden' name='id' value='".$array[$i]->id."'>";
+			echo "<td>".$array[$i]->id."</td>";
+			echo "<td>".$array[$i]->userid."</td>";
+			echo "<td><input name='number_plate' value='".$array[$i]->plate."'></td>";
+			echo "<td><input name='color' value='".$array[$i]->color."'></td>";
+			echo "<td><a href='table.php'>Cancel</a></td>";
+			echo "<td><input type='submit' name='Save' value='Salvesta'></td>";
+			echo "</form>";
+			echo "</tr>";
+			
+		}else{
+			
+			echo "<tr>";
+			echo "<td>".$array[$i]->id."</td>";
+			echo "<td>".$array[$i]->userid."</td>";
+			echo "<td>".$array[$i]->plate."</td>";
+			echo "<td>".$array[$i]->color."</td>";
+			echo "<td><a href='?delete=".$array[$i]->id."'>X</a>";
+			echo "<td><a href='?edit=".$array[$i]->id."'>Edit</a>";
+			echo "</tr>";
+			
+		}
+		
+	}
+	?>
+</table>
